ef8087ed43b1a1edddf5f5f30ed1d0750dbeeda4
Melanie Wachira <melaniemwesh@gmail.com>
2025-08-02 16:30:24 +0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,16 @@
 from flask import Flask
 
 def create_app():
-    print("🛠️ Creating Flask app...")  # Step 1: App creation
+    print("Creating Flask app...")  # Step 1: App creation
     app = Flask(__name__)
 
     # Import and register routes
     from .routes import main as main_blueprint
-    print("🔗 Registering blueprint...")  # Step 2: Registering routes
+    print("Registering blueprint...")  # Step 2: Registering routes
     app.register_blueprint(main_blueprint)
 
     return app
+
 
 
 
